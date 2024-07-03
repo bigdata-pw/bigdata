785b4e4a8751fff2a823404ae056a740f6b88af2
hlky <hlky@hlky.ac>
2024-07-03 14:16:44 +0100

    civitai images

--- civitai/README.md
+++ civitai/README.md
@@ -4,14 +4,25 @@ Civitai API -> MongoDB
 
 ## MongoDB
 
-This script uses database `civitai` and creates collections `lora`, `checkpoint`, `textual_inversion`, `hypernetwork`, `aesthetic_gradient`, `controlnet`, `poses`.
+Uses database `civitai`.
 
-## Usage
+Models script creates collections `lora`, `checkpoint`, `textual_inversion`, `hypernetwork`, `aesthetic_gradient`, `controlnet`, `poses`.
+
+Images script creates collection `images`.
+
+## Usage (models)
 
 - Install `requirements.txt`
 - Fill MongoDB uri
 - Choose `sort`, `period` and `limit`, by default `Oldest`, `AllTime`, `100`
 - Call `get_models`, by default `get_models` is called for all model types
+
+## Usage (images)
+
+- Install `requirements.txt`
+- Fill MongoDB uri
+- Choose `sort`, `period` and `limit`, by default `Newest`, `AllTime`, `100`
+- Call `get_images`
 
 ## Notes
 
---
+++ civitai/images.py
@@ -0,0 +1,67 @@
+import curl_cffi.requests as curl_requests
+from pymongo import InsertOne
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo.collection import Collection
+import random
+import time
+import tqdm
+
+uri = ""
+client = MongoClient(uri, server_api=ServerApi("1"))
+
+SORT = "Newest"
+PERIOD = "AllTime"
+LIMIT = 100
+
+URL = "https://civitai.com/api/v1/images?limit={limit}&period={period}&sort={sort}"
+
+db = client.civitai
+IMAGES = db["images"]
+
+image_ids = set([item["id"] for item in IMAGES.find({})])
+
+pbar = tqdm.tqdm()
+
+
+def get_images(
+    progress_bar: tqdm.tqdm,
+    ids_set: set,
+    collection: Collection,
+    url: str = None,
+):
+    if url is None:
+        url = URL.format(limit=LIMIT, period=PERIOD, sort=SORT)
+        progress_bar.close()
+        progress_bar = tqdm.tqdm()
+    data = curl_requests.get(
+        url,
+        impersonate="chrome",
+        timeout=10,
+    ).json()
+
+    bulk_writes = []
+
+    # received using `Oldest` sort
+    if "error" in data:
+        print(data)
+        return
+    next_page = data["metadata"].get("nextPage", None)
+    next_cursor = data["metadata"].get("nextCursor", None)
+    items = data["items"]
+    for item in items:
+        if item["id"] in ids_set:
+            continue
+        ids_set.add(item["id"])
+        bulk_writes.append(InsertOne(item))
+
+    progress_bar.set_postfix({"nextCursor": str(next_cursor), "count": len(ids_set)})
+    progress_bar.update()
+    if bulk_writes:
+        collection.bulk_write(bulk_writes, ordered=False)
+    if next_page is not None:
+        time.sleep(random.uniform(0.5, 1.5))
+        get_images(progress_bar, ids_set, collection, next_page)
+
+
+get_images(progress_bar=pbar, ids_set=image_ids, collection=IMAGES)
